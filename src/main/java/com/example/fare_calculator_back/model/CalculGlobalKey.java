5d8e6700d112c387c9297252e9345bccd4cedbfa
TAGATSING Samuel <samuelftagat@gmail.com>
2025-08-06 20:36:07 +0100

    Enregistrement d'un calcul

--- src/main/java/com/example/fare_calculator_back/model/CalculGlobalKey.java
+++ src/main/java/com/example/fare_calculator_back/model/CalculGlobalKey.java
@@ -1,11 +1,28 @@
 package com.example.fare_calculator_back.model;
 
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public class CalculGlobalKey {
+@PrimaryKeyClass
+public class CalculGlobalKey implements Serializable {
+
+    @PrimaryKeyColumn(name = "date_calcul", type = PrimaryKeyType.PARTITIONED)
     private LocalDate dateCalcul;
+
+    @PrimaryKeyColumn(name = "id_calcul", type = PrimaryKeyType.CLUSTERED)
     private UUID idCalcul;
+
+    public CalculGlobalKey() {}
+
+    public CalculGlobalKey(LocalDate dateCalcul, UUID idCalcul) {
+        this.dateCalcul = dateCalcul;
+        this.idCalcul = idCalcul;
+    }
 
     public LocalDate getDateCalcul() {
         return dateCalcul;
